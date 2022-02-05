0bc3d5680fe48d8c5f98160abaefdc993a3f5d40
Cedric Tellier <cedric.tellier25@gmail.com>
2022-02-05 22:12:46 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import { IfcViewerAPI } from "web-ifc-viewer";
+
+const container = document.getElementById("viewer-container");
+const viewer = new IfcViewerAPI({ container });
+viewer.addAxes();
+viewer.addGrid();
+
+const input = document.getElementById("file-input");
+
+input.addEventListener(
+    "change",
+
+    async (changed) => {
+        const file = changed.target.files[0];
+        const ifcURL = URL.createObjectURL(file);
+        viewer.IFC.loadIfcUrl(ifcURL);
+
+        console.log(viewer.IFC.getModelID());
+    },
+
+    false
+);
